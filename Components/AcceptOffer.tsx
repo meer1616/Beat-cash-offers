32d6b1ee35154e6f4015b933856d53b0ec8565c9
Meer Patel <meer.spt@gmail.com>
2021-07-19 17:37:04 +0530

    done

--- Components/AcceptOffer.tsx
+++ Components/AcceptOffer.tsx
@@ -8,22 +8,21 @@ export default function AcceptOffer() {
             <div className={styles.thirdPage}>
                 <div className={styles.firstCard}>
                     <div className={styles.CardImg}>
-
-                        <Image height={200} width={240} src="/card1.png" alt="" />
+                        <Image height={200} width={230} src="/card1.png" alt="" />
                     </div>
-                    <div className={styles.cardinfoDiv}>
+                    <div style={{ marginTop: "15px" }} className={styles.cardinfoDiv}>
 
-                        <p style={{ marginTop: "20px", marginBottom: "67px" }}>After connecting with an approval lender*, we will work with them to make sure
-                            that you can submit a Good As Cash Offer </p>
+                        <p >After connecting with an approval lender*, we will work with them to make sure
+                            that you can submit a Good As Cash Offer . Lorem ipsum dolor sit amet  </p>
                     </div>
                 </div>
                 <div className={styles.firstCard}>
                     <div className={styles.CardImg}>
 
-                        <Image height={200} width={240} src="/card2.svg" alt="" />
+                        <Image height={200} width={220} src="/card2.png" alt="" />
                     </div>
                     <div className={styles.cardinfoDiv}>
-                        <p>We&apos;ll help you find the perfect home and write a competitive offer without
+                        <p >We&apos;ll help you find the perfect home and write a competitive offer without
                             a lengthy and unpredicatable financing contingency-giving you an edge over
                             the competition
                         </p>
@@ -35,7 +34,7 @@ export default function AcceptOffer() {
 
                         <Image height={200} width={200} src="/card3.svg" alt="" />
                     </div>
-                    <div className={styles.cardinfoDiv}>
+                    <div style={{ marginTop: "25px" }} className={styles.cardinfoDiv}>
                         <p>Our Good As Cash Offer™ protects your earnest money deposit in the event that your loan proceeds do not arrive in time. You get confidence and the seller gets certainty.
                         </p>
 
--- Components/Homei.tsx
+++ Components/Homei.tsx
@@ -13,7 +13,7 @@ export default function Homei() {
         <Fade left><div className={styles.welcomHomeCont}>
           <h1 className={styles.wlcmTitle}> Give sellers certainity and win with our Good As Cash Offer  <sup style={{ fontSize: "15px" }}>TM</sup></h1>
           <p className={styles.wlcmExtratxt}>
-            we help buyers put their best foot forward by enabling them to submit stronger offers with less risk so they can buy with confidence
+            We help buyers put their best foot forward by enabling them to submit stronger offers with less risk so they can buy with confidence.
           </p>
           <a href="#applynowform"> <button className={styles.applyBtnHome}>   Apply now </button></a>
           <span className={styles.greenTxt}> <FaArrowRight style={{ fontWeight: "lighter", marginTop: "20px" }}></FaArrowRight> Strengthen your offer today</span>
