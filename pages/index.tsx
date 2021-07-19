6885b642c4a5a375144c8879debeb9b3e26ff056
Meer Patel <meer.spt@gmail.com>
2021-07-19 12:54:04 +0530

    meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,12 +25,14 @@ export default function Home() {
       <Head>
         <title>Farehomes</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:title" content="Farehomes" />
-        <meta property="og:description" content="Farehomes Desc" />
-        <meta property="og:image" content={`/logo.svg`} />
+        <meta property="og:title" content="Write A Competitive Letter With Farehomes | Buy With Confidence" />
+        <meta property="og:description" content="Farehomes Will Assist You To Draft An Offer Letter That Can Help You To Buy Home Free From Contingencies | Get Best Home Financing Offer Letter Near You." />
+        <meta property="og:image" content={`/FareHomesLogo.png`} />
+        <meta property="og:image:width" content="100" />
+        <meta property="og:image:height" content="100" />
         <meta property="og:image:alt" content="farehomes" />
-        <meta property="og:image:height" content="200" />
-        <meta property="og:image:width" content="200" />
+        {/* <meta property="og:image:height" content="200" />
+        <meta property="og:image:width" content="200" /> */}
       </Head>
       {<div className={modal ? styles.modalOuter : styles.outerModalNone}><div className={modal ? styles.modalMain : cn(styles.Opacity, styles.modalMain)}>
 
