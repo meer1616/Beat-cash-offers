5a752271f01213a968e671f31b756acbd5107bfe
Meer <meerpatel.1616@gmail.com>
2021-07-24 19:03:23 +0530

    prob

--- Components/AcceptOffer.tsx
+++ Components/AcceptOffer.tsx
@@ -10,7 +10,7 @@ export default function AcceptOffer() {
                     <div className={styles.CardImg}>
                         <Image height={200} width={220} src="/card1new.svg" alt="" />
                     </div>
-                    <div style={{ marginTop: "15px" }} className={styles.cardinfoDiv}>
+                    <div className={styles.cardinfoDiv}>
 
                         <p >After connecting with an approved lender*, we will work with them to make sure that your offer is &quot;as good as cash&quot;, buy getting you fully approved for a loan upfront.</p>
                     </div>
@@ -30,7 +30,7 @@ export default function AcceptOffer() {
                     <div className={styles.CardImg}>
                         <Image height={200} width={220} src="/card3.svg" alt="" />
                     </div>
-                    <div style={{ marginTop: "25px" }} className={styles.cardinfoDiv}>
+                    <div className={styles.cardinfoDiv}>
                         <p>Our Good As Cash Offer™ protects your earnest money deposit in the event that your loan proceeds do not arrive in time. You get confidence and the seller gets certainty.
                         </p>
                     </div>
